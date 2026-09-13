5414723e9669f266e0569480d1d3698cf57f21ba
djtrades421 <dfuentes4211@gmail.com>
2026-09-13 17:18:37 -0500

    Update fsdx-search.js

--- fsdx-search.js
+++ fsdx-search.js
@@ -48,6 +48,7 @@
     {t:'Home',            u:'index.html',       g:'Site', kw:'start landing'},
     {t:'Membership',      u:'memberships.html', g:'Site', kw:'pricing plans monthly quarterly annual trial join'},
     {t:'Nightwing ($9.99)', u:'nightwing.html',   g:'Site', kw:'nightwing cheap entry tools tier site access nexus platform only budget 9.99 journal backtest without indicators'},
+    {t:'Free Indicators',  u:'free-indicators.html', g:'Site', kw:'free arkham orb indicator tradingview no cost gratis freemium try demo script'},
     {t:'Getting Started', u:'setup.html',       g:'Site', kw:'setup install tradingview access onboarding'},
     {t:'Live Sessions',   u:'schedule.html',    g:'Site', kw:'schedule 8:30 morning room gameplan monday'},
     {t:'Knowledge Base',  u:'knowledge.html',   g:'Site', kw:'guides docs walkthrough videos help'},
